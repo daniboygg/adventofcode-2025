acb0e0db773ea3183e51134311a145939826d0bf
daniboygg <dani@daniboygg.dev>
2025-12-01 21:35:39 +0100

    Adapt code to zig 0.15

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of code 2024
 
-Implemented in [zig](https://ziglang.org/) 0.14.1 for https://adventofcode.com/.
+Implemented in [zig](https://ziglang.org/) 0.15.2 for https://adventofcode.com/.
 
 ```bash
 zig run main.zig
--- zig_template.zig
+++ zig_template.zig
@@ -8,14 +8,14 @@ pub fn main() !void {
     const buffer = try std.fs.cwd().readFileAlloc(allocator, "input.txt", 1024 * 1024);
     defer allocator.free(buffer);
 
-    var lines = std.ArrayList([]const u8).init(allocator);
-    defer lines.deinit();
+    var lines: std.ArrayList([]const u8) = .empty;
+    defer lines.deinit(allocator);
     var iter = std.mem.splitSequence(u8, buffer, "\n");
     while (iter.next()) |line| {
         if (line.len == 0) {
             continue;
         }
-        try lines.append(line);
+        try lines.append(allocator, line);
     }
 
     var result = try first(allocator, lines.items);
